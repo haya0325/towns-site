57c42345534ab3861103a6892c2cdb03253f2691
haya0325 <hayato.5038fal@gmail.com>
2019-12-27 18:13:08 +0900

    add ranking

--- app/controllers/stations_controller.rb
+++ app/controllers/stations_controller.rb
@@ -5,6 +5,17 @@ class StationsController < ApplicationController
   # GET /stations.json
   def index
     @stations = Station.all
+    @traffic_station_ids = Comment.get_station_ids(1)
+    @traffic_ranking = @traffic_station_ids.map { |id| Station.find(id) }
+    @commercial_station_ids = Comment.get_station_ids(2)
+    @commercial_ranking = @commercial_station_ids.map { |id| Station.find(id) }
+    @entertainment_station_ids = Comment.get_station_ids(3)
+    @entertainment_ranking = @entertainment_station_ids.map { |id| Station.find(id) }
+    @security_station_ids = Comment.get_station_ids(4)
+    @security_ranking = @security_station_ids.map { |id| Station.find(id) }
+    @nature_station_ids = Comment.get_station_ids(5)
+    @nature_ranking = @nature_station_ids.map { |id| Station.find(id) }
+    @all_ranking = [@traffic_ranking, @commercial_ranking, @entertainment_ranking, @security_ranking, @nature_ranking]
   end
 
   # GET /stations/1
@@ -20,7 +31,7 @@ class StationsController < ApplicationController
     if @station.present?
       redirect_to "/stations/#{@station.id}"
     else
-      redirect_to "/", notice: '駅が見つかりませんでした'
+      redirect_to "/", notice: '見つかりませんでした'
     end
   end
 
@@ -35,3 +46,9 @@ class StationsController < ApplicationController
       params.require(:station).permit(:name)
     end
 end
+
+def ranking
+  product_ids = Review.group(:product_id).order('count_product_id DESC').limit(5).count(:product_id).keys
+  @ranking = product_ids.map { |id| Product.find(id) }
+end
+
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -6,5 +6,7 @@ class Comment < ApplicationRecord
   validates :rate, presence: true
   validates :content, presence: true
 
-
+  def self.get_station_ids(category_id)
+    Comment.where(category_id: category_id).group(:station_id).order('rate DESC').limit(5).count(:station_id).keys
+  end
 end
