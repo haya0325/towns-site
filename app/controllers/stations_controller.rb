8e2a6771c74cbeb43f075635b1809e1c2c7d3aa8
haya0325 <hayato.5038fal@gmail.com>
2019-12-28 16:37:38 +0900

    add ranking method

--- app/controllers/stations_controller.rb
+++ app/controllers/stations_controller.rb
@@ -1,25 +1,15 @@
 class StationsController < ApplicationController
   before_action :set_station, only: :show
 
-  # GET /stations
-  # GET /stations.json
   def index
     @stations = Station.all
-    @traffic_station_ids = Comment.get_station_ids(1)
-    @traffic_ranking = @traffic_station_ids.map { |id| Station.find(id) }
-    @commercial_station_ids = Comment.get_station_ids(2)
-    @commercial_ranking = @commercial_station_ids.map { |id| Station.find(id) }
-    @entertainment_station_ids = Comment.get_station_ids(3)
-    @entertainment_ranking = @entertainment_station_ids.map { |id| Station.find(id) }
-    @security_station_ids = Comment.get_station_ids(4)
-    @security_ranking = @security_station_ids.map { |id| Station.find(id) }
-    @nature_station_ids = Comment.get_station_ids(5)
-    @nature_ranking = @nature_station_ids.map { |id| Station.find(id) }
-    @all_ranking = [@traffic_ranking, @commercial_ranking, @entertainment_ranking, @security_ranking, @nature_ranking]
+    @traffic_ranking = ranking(1)
+    @commercial_ranking = ranking(2)
+    @entertainment_ranking = ranking(3)
+    @security_ranking = ranking(4)
+    @nature_ranking = ranking(5)
   end
 
-  # GET /stations/1
-  # GET /stations/1.json
   def show
     @categories = Category.all
     @each_rate_sum = @station.comments.group(:category_id).sum(:rate)
@@ -37,18 +27,33 @@ class StationsController < ApplicationController
 
   private
     # Use callbacks to share common setup or constraints between actions.
-    def set_station
-      @station = Station.find(params[:id])
+  def set_station
+    @station = Station.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def station_params
+    params.require(:station).permit(:name)
+  end
+
+  def ranking(category_id)
+    hash = {}
+    @stations.each do |station|
+      rate_sum = Comment.where(station_id: station.id, category_id: category_id).sum(:rate)
+      count = Comment.where(station_id: station.id, category_id: category_id).count
+      if count != 0
+        rate_average = (rate_sum/count.to_f).round(1)
+      else
+        rate_average = 0
+      end
+      hash.store(station.id, rate_average)
     end
-
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def station_params
-      params.require(:station).permit(:name)
+    ranking_array = hash.sort_by{ | k ,v | v }.reverse
+    station_id_ranking = []
+    ranking_array.each do |inner_array|
+      station_id_ranking << inner_array[0]
     end
+    station_ranking = station_id_ranking.map{ |id| Station.find(id)}
+    return station_ranking
+  end
 end
-
-def ranking
-  product_ids = Review.group(:product_id).order('count_product_id DESC').limit(5).count(:product_id).keys
-  @ranking = product_ids.map { |id| Product.find(id) }
-end
-
