78cf194c0526ec0803b4726db691ba843a5d0eef
haya0325 <hayato.5038fal@gmail.com>
2020-01-05 11:45:32 +0900

    delete unnecessary files

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -11,14 +11,12 @@ class CommentsController < ApplicationController
     respond_to do |format|
       if @comment.save
         format.html { redirect_to "/stations/#{@comment.station_id}", notice: 'レビューが投稿されました' }
-
       else
         format.html { render :new }
         format.json { render json: @comment.errors, status: :unprocessable_entity }
       end
     end
   end
-
 
   private
   def set_station
